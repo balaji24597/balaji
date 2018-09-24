2f95f25d9e58cbff68d4fd46e39d488a151c4820
balaji24597 <42768840+balaji24597@users.noreply.github.com>
2018-09-24 14:54:29 +0530

    Create balaji13.java

---
+++ balaji13.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class j6 
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int num;
+		num=sc.nextInt();
+		if(num%2 ==0)
+		{
+			System.out.println(num +" is an EVEN number.");
+		}
+		else
+		{
+			System.out.println(num +"invalid");
+		}
+	}
+}
